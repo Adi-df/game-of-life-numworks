bad162ffc6852c6fe7874d63c26502b364320b38
Adi-df <adidf-web@laposte.net>
2022-08-16 19:44:49 +0200

    Move pointer and set tiles

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 pub mod eadk;
-use eadk::{display, timing, Color, Rect, SCREEN_HEIGHT, SCREEN_WIDTH};
+use eadk::{display, key, keyboard, timing, Color, Rect, SCREEN_HEIGHT, SCREEN_WIDTH};
 
 use heapless::Vec;
 
@@ -18,7 +18,7 @@ pub static EADK_APP_API_LEVEL: u32 = 0;
 #[link_section = ".rodata.eadk_app_icon"]
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
 
-const CELL_SIZE: u16 = 2;
+const CELL_SIZE: u16 = 4;
 const LINE_SIZE: usize = (SCREEN_WIDTH / CELL_SIZE) as usize;
 const COLUMN_SIZE: usize = (SCREEN_HEIGHT / CELL_SIZE) as usize;
 const BOARD_SIZE: usize = LINE_SIZE * COLUMN_SIZE;
@@ -57,6 +57,8 @@ fn _eadk_main() {
     let mut on_board: OnBoard = Vec::new();
 
     loop {
+        let keyboard_state = keyboard::scan();
+
         draw_board(&on_board);
 
         match state {
@@ -71,12 +73,36 @@ fn _eadk_main() {
                     },
                     Color::RED,
                 );
+
+                if keyboard_state.key_down(key::EXE) {
+                    let current = &mut board[pointer.0 as usize + pointer.1 as usize * LINE_SIZE];
+                    match current {
+                        false => {
+                            on_board.push(pointer).unwrap();
+                        }
+                        true => {
+                            on_board.retain(|p| *p != pointer);
+                        }
+                    }
+                    *current = !*current;
+                }
+
+                if keyboard_state.key_down(key::UP) {
+                    pointer.1 -= 1;
+                } else if keyboard_state.key_down(key::DOWN) {
+                    pointer.1 += 1;
+                }
+                if keyboard_state.key_down(key::LEFT) {
+                    pointer.0 -= 1;
+                } else if keyboard_state.key_down(key::RIGHT) {
+                    pointer.0 += 1;
+                }
             }
             AppState::Running => {}
             AppState::StepByStep => {}
         }
 
         display::wait_for_vblank();
-        timing::msleep(300);
+        timing::msleep(100);
     }
 }
