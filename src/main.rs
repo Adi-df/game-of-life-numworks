5f47b2823b2b127a04d310a31ccdccf997130e90
Adi-df <adidf-web@laposte.net>
2022-08-16 19:00:23 +0200

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,5 @@ pub static EADK_APP_API_LEVEL: u32 = 0;
 #[link_section = ".rodata.eadk_app_icon"]
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
 
-fn random_u16() -> u16 {
-    return eadk::random() as u16;
-}
-
-fn random_coordinate() -> u16 {
-    return (eadk::random() % 0xFF) as u16;
-}
-
 #[no_mangle]
-fn _eadk_main() {
-    for _ in 0..100 {
-        let c = eadk::Color {
-            rgb565: random_u16(),
-        };
-        let r = eadk::Rect {
-            x: random_coordinate(),
-            y: random_coordinate(),
-            width: random_coordinate(),
-            height: random_coordinate(),
-        };
-        eadk::display::push_rect_uniform(r, c);
-    }
-    loop {}
-}
+fn _eadk_main() {}
