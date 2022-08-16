6f20e29c436a6fde2e69c3925d52426518d76c54
Adi-df <adidf-web@laposte.net>
2022-08-16 19:02:10 +0200

    Define game board size consts

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 pub mod eadk;
+use eadk::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
 #[export_name = "eadk_app_name"]
 #[link_section = ".rodata.eadk_app_name"]
@@ -14,6 +15,10 @@ pub static EADK_APP_API_LEVEL: u32 = 0;
 #[export_name = "eadk_app_icon"]
 #[link_section = ".rodata.eadk_app_icon"]
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
+
+const CELL_SIZE: u16 = 2;
+const LINE_CELL: u16 = SCREEN_WIDTH / CELL_SIZE;
+const COLUMN_CELL: u16 = SCREEN_HEIGHT / CELL_SIZE;
 
 #[no_mangle]
 fn _eadk_main() {}
