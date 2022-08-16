43a704c5da8af73554e2fc727642299ccbc72a10
Adi-df <adidf-web@laposte.net>
2022-08-16 19:12:22 +0200

    Create board and app state

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 pub mod eadk;
 use eadk::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use heapless::Vec;
 
 #[export_name = "eadk_app_name"]
 #[link_section = ".rodata.eadk_app_name"]
@@ -17,8 +18,9 @@ pub static EADK_APP_API_LEVEL: u32 = 0;
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
 
 const CELL_SIZE: u16 = 2;
-const LINE_CELL: u16 = SCREEN_WIDTH / CELL_SIZE;
-const COLUMN_CELL: u16 = SCREEN_HEIGHT / CELL_SIZE;
+const LINE_SIZE: u16 = SCREEN_WIDTH / CELL_SIZE;
+const COLUMN_SIZE: u16 = SCREEN_HEIGHT / CELL_SIZE;
+const BOARD_SIZE: usize = LINE_SIZE as usize * COLUMN_SIZE as usize;
 
 enum AppState {
     Editor,
@@ -27,4 +29,7 @@ enum AppState {
 }
 
 #[no_mangle]
-fn _eadk_main() {}
+fn _eadk_main() {
+    let mut state: AppState = AppState::Editor;
+    let mut board: [bool; BOARD_SIZE] = [false; BOARD_SIZE];
+}
